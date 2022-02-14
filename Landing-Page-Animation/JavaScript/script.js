b3a5f86757bf993736ada865432d4e3fe25b84a8
Pritam Bera <pritambera440@gmail.com>
2022-02-14 22:17:08 +0530

    Initial Release

--- Landing-Page-Animation/JavaScript/script.js
+++ Landing-Page-Animation/JavaScript/script.js
@@ -1,6 +1,7 @@
-
-
-console.log("hello");
-
-// gsap.to("#logo",{color:"green",duration:2});
-gsap.to("#logo", {duration: 1, x: 100});
+let t1 = gsap.timeline();
+t1.from('.bg',{scale:0.1,opacity:0.5,duration:3,ease: Expo.easeOut,delay:0.2})
+  .to('.text-animation', { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', y:0, stagger: .3, duration: 1}, "-=2.9")
+  .to('.text-animation', { clipPath: 'polygon(0 0, 100% 0, 100% 0, 0 0)', y: -200, duration: .2, delay: .5})
+  .to('.text-animation', { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', stagger: .3, duration: .3, delay: .5})
+  .to('svg',{clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',y:0})
+  .to('.grid-3',{clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',y:0,stagger:.3,opacity:1,duration:1}, "-=1.4" )
